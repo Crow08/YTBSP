4204d6b106668e4445f7353e5a65ade65b7b6054
Aaron Olszewski <aaronolszewski2@googlemail.com>
2020-08-22 16:58:57 +0200

    Switch from TSLint to ESLint because TSLint is deprecated.
    Also make all configs declare ES2020 feature set.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2020": true,
+        "greasemonkey": true,
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": 2020,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "indent": ["error", 4],
+        "quotes": ["error", "double"],
+        "semi": ["error", "always"]
+    }
+};
