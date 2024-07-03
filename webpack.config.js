3f4b8c381c06f16ce6cc46e7f488c149d35f1403
Kevin Hertfelder <kevin.game@web.de>
2024-07-03 21:18:43 +0200

    Migrate to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
 // eslint-disable-next-line no-undef
+const webpack = require('webpack')
 module.exports = {
     resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"]
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "vm": require.resolve("vm-browserify"),
+            "buffer": require.resolve("buffer"),
+            "querystring": require.resolve("querystring-es3"),
+            "stream": require.resolve("stream-browserify"),
+            "url": require.resolve("url")
+
+        }
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+            Buffer: ["buffer", "Buffer"]
+        })
+
+    ],
     mode: "development", // TODO: switch to production
     module: {
         rules: [
